a850289c91e8441c5f5f3a597f1d25f73ead64a5
kirin-bupt <jiweibin@gmail.com>
2026-05-19 13:24:21 +0800

    提交chapter 3

--- chapter-3/agent3-1.py
+++ chapter-3/agent3-1.py
@@ -1,16 +1,29 @@
-from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
 from dotenv import load_dotenv
 
+import os
+
+# 修复：显式加载项目根目录的 .env
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# load_dotenv(os.path.join(PROJECT_ROOT, ".env"))
 load_dotenv()
 
-chat_model = ChatOpenAI(model="gpt-4o-mini")
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-while True:
-    user_input = input("You:> ")
-    if user_input.lower() == "exit":
-        break
-    stream = chat_model.stream([HumanMessage(content=user_input)])
-    for chunk in stream:
-        print(chunk.content, end="", flush=True)
-    print()
+loader = TextLoader(os.path.join(SCRIPT_DIR, "geektime-article-821470.txt"))
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma(
+    collection_name="ai_learning",
+    embedding_function=OpenAIEmbeddings(model="text-embedding-3-small"),
+    persist_directory=os.path.join(SCRIPT_DIR, "vectordb"),
+)
+vectorstore.add_documents(splits)
+
+documents = vectorstore.similarity_search("专栏的作者是谁？")
+print(documents)
--- chapter-3/readme.md
+++ chapter-3/readme.md
@@ -1,3 +1,4 @@
 # 对应极客时间课程-第11课
 ## 课程链接：https://time.geekbang.org/column/article/827564
-- agent3-1：最简单的无记忆的agent
+- agent3-1：RAG index并测试
+- agent3-2：RAG 检索增强生成
